cd90877a40dd3718100879d58dd1372d4a9af3d9
habeshawit <shalomseleshi@gmail.com>
2021-02-09 06:42:25 -0500

    commit tree 145c63bee4383ce72e15a5ee57ec5641d17bf851

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift()
+}
+
+function  appendDriver(name) {
+    const newDriver = [ ...drivers, name];
+    return newDriver;
+}
+
+function prependDriver(name){
+    const newDriver = [ name, ...drivers];
+    return newDriver;
+}
+
+function removeLastDriver(){
+    const newDrivers = drivers.slice(0, drivers.length - 1);
+    return newDrivers;
+}
+
+function removeFirstDriver() {
+    const newDrivers = drivers.slice(1);
+    return newDrivers;
+}
